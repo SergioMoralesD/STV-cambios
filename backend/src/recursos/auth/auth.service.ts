e1ed03988e07b417f11b7e72cee47f408cc14a95
samu-gonz <samuelgonz2006@gmail.com>
2026-05-26 12:26:34 +0100

    Update auth.service.ts

--- backend/src/recursos/auth/auth.service.ts
+++ backend/src/recursos/auth/auth.service.ts
@@ -1,1 +1,15 @@
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
+@Controller('auth') // La URL base será: tu-dominio.com/auth
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login') // La URL final para la web será: tu-dominio.com/auth/login
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() body: any) {
+    // Recogemos el usuario y clave que se escriben en el formulario de la web
+    const { usuario, clave } = body;
+    return await this.authService.login(usuario, clave);
+  }
+}
