ac7c92d4e750b088dce958bea3f0d9fc319b3f93
samu-gonz <samuelgonz2006@gmail.com>
2026-05-26 12:29:08 +0100

    Update auth.module.ts

--- backend/src/recursos/auth/auth.module.ts
+++ backend/src/recursos/auth/auth.module.ts
@@ -1,1 +1,17 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true, // Hace que la protección JWT sirva para todo el backend
+      secret: 'CLAVE_SECRETA_STV_DE_PRUEBAS', // Sello de seguridad
+      signOptions: { expiresIn: '8h' }, // La sesión del técnico expira a las 8 horas
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
