2700d8837a8d209e6b1ac15dae1486a598d0a325
samu-gonz <samuelgonz2006@gmail.com>
2026-05-27 10:17:05 +0100

    Update auth.service.ts

--- backend/src/recursos/auth/auth.service.ts
+++ backend/src/recursos/auth/auth.service.ts
@@ -1,13 +1,21 @@
-async login(usuario: string, clave: string) {
-  if (usuario === 'admin' && clave === 'stv2026') {
-    const payload = { username: usuario, role: 'admin' };
-    return {
-      backend_status: 'AUTHENTICATED',
-      // ¡Aquí añadimos la clave secreta igual que en el Guard!
-      accessToken: await this.jwtService.signAsync(payload, {
-        secret: 'stv_2026'
-      }),
-    };
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async login(usuario: string, clave: string) {
+    if (usuario === 'admin' && clave === 'stv2026') {
+      const payload = { username: usuario, role: 'admin' };
+      return {
+        backend_status: 'AUTHENTICATED',
+        // Usamos la clave secreta aquí
+        accessToken: await this.jwtService.signAsync(payload, {
+          secret: 'stv_2026'
+        }),
+      };
+    }
+    return null;
   }
-  return null;
 }
