7f79ebbfd38cb2d760a5109eaa3f5ee3249d4293
samu-gonz <samuelgonz2006@gmail.com>
2026-05-27 07:59:21 +0100

    Update recursos.controller.ts

--- backend/src/recursos/recursos.controller.ts
+++ backend/src/recursos/recursos.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { RecursosService } from './recursos.service';
-import { AuthGuard } from '../auth/guards/auth.guard'; 
+import { AuthGuard } from './auth/guards/auth.guard'; 
 
 @Controller('recursos') 
 export class RecursosController {
   constructor(private readonly recursosService: RecursosService) {}
 
   @Get('datos') 
-  @UseGuards(AuthGuard) // 🔒 Bloquea el paso si no hay login correcto
+  @UseGuards(AuthGuard) // 🔒 Bloquea el paso si el técnico no está logueado
   async obtenerDatos() {
     return await this.recursosService.obtenerDatosDeAPI();
   }
