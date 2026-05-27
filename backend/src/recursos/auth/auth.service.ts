debf14506258795c77a5f314a05dde2a372d8ecd
samu-gonz <samuelgonz2006@gmail.com>
2026-05-27 10:39:06 +0100

    Update auth.service.ts

--- backend/src/recursos/auth/auth.service.ts
+++ backend/src/recursos/auth/auth.service.ts
@@ -5,17 +5,17 @@ import { JwtService } from '@nestjs/jwt';
 export class AuthService {
   constructor(private jwtService: JwtService) {}
 
-  async login(usuario: string, clave: string) {
-    if (usuario === 'admin' && clave === 'stv2026') {
-      const payload = { username: usuario, role: 'admin' };
-      return {
-        backend_status: 'AUTHENTICATED',
-        // Usamos la clave secreta aquí
-        accessToken: await this.jwtService.signAsync(payload, {
-          secret: 'stv_2026'
-        }),
-      };
-    }
-    return null;
+ async login(usuario: string, clave: string) {
+  // Esto es VITAL: mira tu terminal de VS Code después de hacer Send
+  console.log("VALORES RECIBIDOS -> Usuario: '" + usuario + "', Clave: '" + clave + "'");
+
+  if (usuario === 'admin' && clave === 'stv2026') {
+    const payload = { username: usuario, role: 'admin' };
+    return {
+      backend_status: 'AUTHENTICATED',
+      accessToken: await this.jwtService.signAsync(payload, { secret: 'stv_2026' }),
+    };
   }
+  
+  return null;
 }
