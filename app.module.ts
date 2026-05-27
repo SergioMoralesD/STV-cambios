4edc835fa6f0d6ed22c03546b92e0f6017110201
samu-gonz <samuelgonz2006@gmail.com>
2026-05-27 07:59:58 +0100

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -4,8 +4,8 @@ import { RecursosModule } from './backend/src/recursos/recursos.module';
 
 @Module({
   imports: [
-    AuthModule,     // 🔒 Conecta tu Login real
-    RecursosModule, // 📊 Conecta el lector del JSON de averías
+    AuthModule,     // 🔒 Activa el Login de STV
+    RecursosModule, // 📊 Activa el lector de averías del JSON
   ],
 })
 export class AppModule {}
